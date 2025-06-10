76340410865936aa7a4bd199dd9da5e254dc5eda
kofi-k <kofivincent026@gmail.com>
2025-06-10 18:41:04 +0000

    add HessenPoort subdomain

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -2,7 +2,6 @@ package com.zenmo.web.zenmo.domains.lux.subdomains
 
 import androidx.compose.runtime.Composable
 import com.zenmo.web.zenmo.domains.lux.subdomains.drechtsteden.DrechtstedenRouting
-import com.zenmo.web.zenmo.domains.lux.subdomains.drechtsteden.pages.DrechtstedenContent
 import com.zenmo.web.zenmo.domains.lux.subdomains.genius.GeniusIndex
 
 
@@ -17,6 +16,6 @@ fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
     when (subdomain) {
         LuxSubdomains.DRECHTSTEDEN -> DrechtstedenRouting()
         LuxSubdomains.GENIUS -> GeniusIndex()
-        LuxSubdomains.HESSENPOORT -> DrechtstedenContent(subdomain.domainName)
+        LuxSubdomains.HESSENPOORT -> HessenPoortIndex()
     }
 }
