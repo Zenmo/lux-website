6431f46f9be295c12a362b7e7b1167367b5247a1
kofi-k <kofivincent026@gmail.com>
2025-06-11 23:28:11 +0000

    add Nederland subdomain

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -12,6 +12,7 @@ enum class LuxSubdomains(val domainName: String) {
     GENIUS("genius"),
     HESSENPOORT("hessenpoort"),
     BRABANT("brabant"),
+    NEDERLAND("nederland"),
 }
 
 @Composable
@@ -21,5 +22,6 @@ fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
         LuxSubdomains.GENIUS -> GeniusIndex()
         LuxSubdomains.HESSENPOORT -> DrechtstedenContent(subdomain.domainName)
         LuxSubdomains.BRABANT -> BrabantRouting()
+        LuxSubdomains.NEDERLAND -> NederlandIndex()
     }
 }
