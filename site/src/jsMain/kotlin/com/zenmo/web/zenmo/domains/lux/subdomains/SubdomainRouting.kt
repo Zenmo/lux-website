96a7582a57872db725bd83b8650a289ec31747a4
kofi-k <kofivincent026@gmail.com>
2025-06-11 23:36:38 +0000

    add Bunderbuurten subdomain

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -12,6 +12,7 @@ enum class LuxSubdomains(val domainName: String) {
     GENIUS("genius"),
     HESSENPOORT("hessenpoort"),
     BRABANT("brabant"),
+    BUNDERBUURTEN("bunderbuurten"),
 }
 
 @Composable
@@ -21,5 +22,6 @@ fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
         LuxSubdomains.GENIUS -> GeniusIndex()
         LuxSubdomains.HESSENPOORT -> DrechtstedenContent(subdomain.domainName)
         LuxSubdomains.BRABANT -> BrabantRouting()
+        LuxSubdomains.BUNDERBUURTEN -> BunderbuurtenIndex()
     }
 }
