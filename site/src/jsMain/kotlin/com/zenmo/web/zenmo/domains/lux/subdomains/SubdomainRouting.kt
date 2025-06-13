23b73edb2ece5336ed1703156002edceca63864d
kofi-k <kofivincent026@gmail.com>
2025-06-13 15:55:28 +0000

    genius behind access policy

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -3,7 +3,6 @@ package com.zenmo.web.zenmo.domains.lux.subdomains
 import androidx.compose.runtime.Composable
 import com.zenmo.web.zenmo.domains.lux.subdomains.brabant.BrabantRouting
 import com.zenmo.web.zenmo.domains.lux.subdomains.drechtsteden.DrechtstedenRouting
-import com.zenmo.web.zenmo.domains.lux.subdomains.genius.GeniusIndex
 import com.zenmo.web.zenmo.protected.ProtectedWrapper
 
 
@@ -25,7 +24,7 @@ enum class LuxSubdomains(val domainName: String) {
 fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
     when (subdomain) {
         LuxSubdomains.DRECHTSTEDEN -> DrechtstedenRouting()
-        LuxSubdomains.GENIUS -> GeniusIndex()
+        LuxSubdomains.GENIUS -> ProtectedWrapper("genius")
         LuxSubdomains.HESSENPOORT -> ProtectedWrapper("hessenpoort")
         LuxSubdomains.BRABANT -> BrabantRouting()
         LuxSubdomains.NEDERLAND -> NederlandIndex()
