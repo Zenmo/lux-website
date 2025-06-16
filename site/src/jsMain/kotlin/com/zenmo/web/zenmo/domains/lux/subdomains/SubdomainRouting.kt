d007e189b4607a3d52f583a0133ec70b103f1d1a
kofi-k <kofivincent026@gmail.com>
2025-06-16 13:42:46 +0000

    add Vruchtenbuurt subdomain (#116)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -16,6 +16,7 @@ enum class LuxSubdomains(val domainName: String) {
     HILVERSUM("hilversum"),
     BUNDERBUURTEN("bunderbuurten"),
     LOENEN("loenen"),
+    VRUCHTENBUURT("vruchtenbuurt"),
 }
 
 @Composable
@@ -29,5 +30,6 @@ fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
         LuxSubdomains.HILVERSUM -> HilversumIndex()
         LuxSubdomains.BUNDERBUURTEN -> BunderbuurtenIndex()
         LuxSubdomains.LOENEN -> LoenenIndex()
+        LuxSubdomains.VRUCHTENBUURT -> VruchtenbuurtIndex()
     }
 }
