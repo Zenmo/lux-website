fb01aad418afd73c0dcf1528d22a6457f5e3e302
kofi-k <kofivincent026@gmail.com>
2025-06-17 12:18:50 +0000

    genius behind access policy (#121)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/protected/entrypoints/genius/ProtectedComponent.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/protected/entrypoints/genius/ProtectedComponent.kt
@@ -1,20 +1,92 @@
 package com.zenmo.web.zenmo.protected.entrypoints.genius
 
 import androidx.compose.runtime.Composable
-import com.zenmo.web.zenmo.components.widgets.LangText
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiMail
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiPhone
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.shapes.Circle
+import com.varabyte.kobweb.silk.theme.shapes.clip
+import com.zenmo.web.zenmo.components.widgets.MediaContentLayout
+import com.zenmo.web.zenmo.components.widgets.SectionContainer
+import com.zenmo.web.zenmo.domains.lux.components.layout.LuxSubdomainPageLayout
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
+import com.zenmo.web.zenmo.domains.zenmo.sections.team.TeamCardImageContainerStyle
+import com.zenmo.web.zenmo.domains.zenmo.sections.team.ZenmoTeam
+import com.zenmo.web.zenmo.domains.zenmo.widgets.anylogic.AnyLogicEmbed
+import com.zenmo.web.zenmo.theme.SitePalette
 import energy.lux.site.shared.AccessPolicy
-import org.jetbrains.compose.web.dom.Span
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.dom.Text
+import kotlin.uuid.Uuid
 
 @JsExport
-val accessPolicy = AccessPolicy.RoleBased("GENIUS")
+val accessPolicy = AccessPolicy.RoleBased("Genius")
 
 @JsExport
 @Composable
 fun ProtectedComponent() {
-    Span {
-        LangText(
-            en = "Very secret text just for GENIUS",
-            nl = "Geheime text alleen voor GENIUS",
-        )
+    LuxSubdomainPageLayout(
+        title = "Genius"
+    ) {
+        SectionContainer(
+            modifier = Modifier.gap(5.cssRem).margin(bottom = 5.cssRem),
+        ) {
+            AnyLogicEmbed(
+                modelId = Uuid.parse("42e7ad8d-102f-4a32-808c-65bdd646a52a"),
+                apiKey = Uuid.parse("6764ad7a-0c33-4a0e-bd98-8255e8966419"),
+                modifier = Modifier.maxWidth(90.cssRem).margin(topBottom = 4.cssRem)
+            )
+
+            MediaContentLayout(
+                imageUrl = "",
+                visualContent = {
+                    Box(TeamCardImageContainerStyle.toModifier().size(22.cssRem)) {
+                        Image(
+                            modifier = Modifier.fillMaxSize().objectFit(ObjectFit.Cover).clip(Circle()),
+                            src = ZenmoTeam.ATE.image,
+                            alt = "${ZenmoTeam.ATE.memberName} photo",
+                        )
+                    }
+                },
+                title = {
+                    HeaderText(
+                        enText = ZenmoTeam.ATE.memberName,
+                        nlText = ZenmoTeam.ATE.memberName,
+                        modifier = Modifier.margin(0.cssRem)
+                    )
+                },
+                subtitle = {
+                    SubHeaderText(
+                        enText = "Website and model development",
+                        nlText = "Website en model ontwikkeling",
+                        modifier = Modifier.color(SitePalette.light.primary)
+                            .margin(0.cssRem)
+                    )
+                },
+                description = {
+                    Column(Modifier.gap(0.5.cssRem)) {
+                        Row {
+                            MdiPhone(Modifier.padding(right = 0.25.cssRem))
+                            Text("+31 6 14910380")
+                        }
+
+                        Row {
+                            MdiMail(Modifier.padding(right = 0.25.cssRem))
+                            Text(ZenmoTeam.ATE.email)
+                        }
+                    }
+                },
+                actionText = {},
+                reversed = false,
+            )
+        }
     }
 }
