b554de67e7afec679ac267ac96c0b4230fda8d40
Kofi K. <kofivincent026@gmail.com>
2025-06-27 14:54:59 +0200

    - update document title based on domain - refactor PageLayout to use custom header and footer components - wrap LuxSubdomainPageLayout with PageLayout

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/components/layout/LuxSubdomainPageLayout.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/components/layout/LuxSubdomainPageLayout.kt
@@ -2,6 +2,7 @@ package com.zenmo.web.zenmo.domains.lux.components.layout
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
@@ -10,6 +11,7 @@ import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toModifier
+import com.zenmo.web.zenmo.components.layouts.PageLayout
 import com.zenmo.web.zenmo.components.widgets.LangText
 import com.zenmo.web.zenmo.components.widgets.SectionContainer
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
@@ -50,44 +52,49 @@ fun LuxSubdomainPageLayout(
     title: String,
     enSubtitle: String = "Design your own energy system",
     nlSubtitle: String = "Ontwerp je eigen energiesysteem",
-    content: @Composable () -> Unit,
+    content: @Composable ColumnScope.() -> Unit,
 ) {
-    Column(Modifier.fillMaxSize()) {
-        SectionContainer(
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier
-                .background(SitePalette.light.primary)
-                .color(SitePalette.light.onPrimary)
-                .then(LayoutHeaderStyle.toModifier()),
-        ) {
-            val protocol = window.location.protocol
-            A(
-                href = "$protocol//${SiteGlobals.LUX_DOMAIN}",
+    PageLayout(
+        header = {
+            SectionContainer(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .background(SitePalette.light.primary)
+                    .color(SitePalette.light.onPrimary)
+                    .then(LayoutHeaderStyle.toModifier()),
             ) {
-                Image(
-                    src = "/lux/logos/lux-sun-logo.svg",
-                    width = 60,
-                    height = 60,
-                )
-            }
-            Column {
-                HeaderText(
-                    enText = title,
-                    nlText = title,
-                    modifier = TextStyle.toModifier(HolonBlockHeaderTextStyle)
-                        .margin(0.cssRem)
-                )
-                Span(
-                    TextStyle.toModifier(HolonLineTextStyle)
-                        .margin(0.cssRem).toAttrs()
+                val protocol = window.location.protocol
+                A(
+                    href = "$protocol//${SiteGlobals.LUX_DOMAIN}",
                 ) {
-                    LangText(
-                        en = enSubtitle,
-                        nl = nlSubtitle,
+                    Image(
+                        src = "/lux/logos/lux-sun-logo.svg",
+                        width = 60,
+                        height = 60,
                     )
                 }
+                Column {
+                    HeaderText(
+                        enText = title,
+                        nlText = title,
+                        modifier = TextStyle.toModifier(HolonBlockHeaderTextStyle)
+                            .margin(0.cssRem)
+                    )
+                    Span(
+                        TextStyle.toModifier(HolonLineTextStyle)
+                            .margin(0.cssRem).toAttrs()
+                    ) {
+                        LangText(
+                            en = enSubtitle,
+                            nl = nlSubtitle,
+                        )
+                    }
+                }
             }
-        }
+        },
+        footer = {},
+        title = title
+    ) {
         content()
     }
 }
