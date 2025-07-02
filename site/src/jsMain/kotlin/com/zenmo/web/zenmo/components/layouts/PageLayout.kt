60eb16988cae32b6279d6b5b87918ec3f7e69182
kofi-k <kofivincent026@gmail.com>
2025-07-02 14:38:42 +0000

    - update document title based on domain (#129)
    - refactor PageLayout to use custom header and footer components
    - wrap LuxSubdomainPageLayout with PageLayout

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/layouts/PageLayout.kt
@@ -10,15 +10,23 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.zenmo.web.zenmo.domains.zenmo.sections.Footer
 import com.zenmo.web.zenmo.domains.zenmo.sections.nav_header.NavHeader
+import com.zenmo.web.zenmo.pages.SiteGlobals
+import com.zenmo.web.zenmo.pages.isLocalOrPreviewEnvironment
 import kotlinx.browser.document
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.fr
 import org.jetbrains.compose.web.css.percent
 
 
 @Composable
-fun PageLayout(title: String, content: @Composable ColumnScope.() -> Unit) {
+fun PageLayout(
+    title: String,
+    header: @Composable () -> Unit = { NavHeader() },
+    footer: @Composable () -> Unit = { Footer(Modifier.fillMaxWidth().gridRow(2)) },
+    content: @Composable ColumnScope.() -> Unit,
+) {
     LaunchedEffect(title) {
-        document.title = "Zenmo - $title"
+        document.title = documentTitleByDomain(title)
     }
     Box(
         Modifier
@@ -31,9 +39,23 @@ fun PageLayout(title: String, content: @Composable ColumnScope.() -> Unit) {
             Modifier.fillMaxSize().gridRow(1),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            NavHeader()
+            header()
             content()
         }
-        Footer(Modifier.fillMaxWidth().gridRow(2))
+        footer()
+    }
+}
+
+private fun documentTitleByDomain(extraTitle: String): String {
+    val domain = window.location.host
+    val previewText = when (isLocalOrPreviewEnvironment()) {
+        true -> "Preview | "
+        false -> ""
+    }
+    return when {
+        domain == SiteGlobals.ZENMO_DOMAIN -> "${previewText}Zenmo - $extraTitle"
+        domain == SiteGlobals.LUX_DOMAIN -> "${previewText}LUX Energy"
+        domain.endsWith(".${SiteGlobals.LUX_DOMAIN}") -> "$extraTitle | LUX Energy"
+        else -> "Unknown"
     }
 }
