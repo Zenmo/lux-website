e0ceff4b165e51ee701e718f6b81f426652c0c72
Kofi K. <kofivincent026@gmail.com>
2025-07-03 04:28:42 +0200

    page for lux sections

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/products/LuxProducts.kt
@@ -0,0 +1,15 @@
+package com.zenmo.web.zenmo.domains.lux.sections.products
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.id
+import com.zenmo.web.zenmo.domains.lux.sections.nav_header.LuxSection
+
+@Composable
+fun LuxProducts() {
+    Column(Modifier.id(LuxSection.PRODUCTS.id)) {
+        LuxResidentialArea()
+        LuxMunicipality()
+    }
+}
