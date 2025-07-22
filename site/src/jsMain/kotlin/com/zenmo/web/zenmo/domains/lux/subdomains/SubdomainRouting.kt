65f22a22c64d6fa0aa0a3a19e70a41d9fac3fe19
kofi-k <kofivincent026@gmail.com>
2025-07-22 15:53:23 +0000

    genius-project (#164)
    * wrapper for anylogic model behind access policy
    
    * genius project page

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/SubdomainRouting.kt
@@ -3,6 +3,7 @@ package com.zenmo.web.zenmo.domains.lux.subdomains
 import androidx.compose.runtime.Composable
 import com.zenmo.web.zenmo.domains.lux.subdomains.brabant.BrabantRouting
 import com.zenmo.web.zenmo.domains.lux.subdomains.drechtsteden.DrechtstedenRouting
+import com.zenmo.web.zenmo.domains.lux.subdomains.genius.Genius
 import com.zenmo.web.zenmo.protected.ProtectedWrapper
 
 
@@ -24,7 +25,7 @@ enum class LuxSubdomains(val domainName: String) {
 fun LuxSubdomainRoutingComponent(subdomain: LuxSubdomains) {
     when (subdomain) {
         LuxSubdomains.DRECHTSTEDEN -> DrechtstedenRouting()
-        LuxSubdomains.GENIUS -> ProtectedWrapper("genius")
+        LuxSubdomains.GENIUS -> Genius()
         LuxSubdomains.HESSENPOORT -> ProtectedWrapper("hessenpoort")
         LuxSubdomains.BRABANT -> BrabantRouting()
         LuxSubdomains.NEDERLAND -> NederlandIndex()
