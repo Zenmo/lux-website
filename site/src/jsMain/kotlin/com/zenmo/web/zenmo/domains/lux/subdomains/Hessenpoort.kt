f0fc6cb37b3a51c0f4a1ac190f304541ebaeda33
Kofi K. <kofivincent026@gmail.com>
2025-07-24 10:53:29 +0200

    use ModeWrapper for private models - hessenpoort - kasalsenergiebron

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/Hessenpoort.kt
@@ -0,0 +1,88 @@
+package com.zenmo.web.zenmo.domains.lux.subdomains
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiMail
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiPhone
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.shapes.Circle
+import com.varabyte.kobweb.silk.theme.shapes.clip
+import com.zenmo.web.zenmo.components.widgets.MediaContentLayout
+import com.zenmo.web.zenmo.components.widgets.SectionContainer
+import com.zenmo.web.zenmo.domains.lux.components.layout.LuxSubdomainPageLayout
+import com.zenmo.web.zenmo.domains.lux.sections.LuxSectionContainerStyleVariant
+import com.zenmo.web.zenmo.domains.lux.subdomains.genius.ModelWrapper
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
+import com.zenmo.web.zenmo.domains.zenmo.sections.team.TeamCardImageContainerStyle
+import com.zenmo.web.zenmo.domains.zenmo.sections.team.ZenmoTeam
+import com.zenmo.web.zenmo.theme.SitePalette
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.dom.Text
+
+@Composable
+fun Hessenpoort() {
+    LuxSubdomainPageLayout(
+        title = "Hessenpoort",
+    ) {
+        SectionContainer(
+            modifier = Modifier.gap(5.cssRem),
+            variant = LuxSectionContainerStyleVariant
+        ) {
+
+            ModelWrapper(
+                imgUrl = "/lux/images/models/hessenpoort.png",
+                entryPoint = "hessenpoort",
+            )
+
+            MediaContentLayout(
+                imageUrl = "",
+                visualContent = {
+                    Box(TeamCardImageContainerStyle.toModifier().size(22.cssRem)) {
+                        Image(
+                            modifier = Modifier.fillMaxSize().objectFit(ObjectFit.Cover).clip(Circle()),
+                            src = ZenmoTeam.ATE.image,
+                            alt = "${ZenmoTeam.ATE.memberName} photo",
+                        )
+                    }
+                },
+                title = {
+                    HeaderText(
+                        enText = ZenmoTeam.ATE.memberName,
+                        nlText = ZenmoTeam.ATE.memberName,
+                        modifier = Modifier.margin(0.cssRem)
+                    )
+                },
+                subtitle = {
+                    SubHeaderText(
+                        enText = "Website and model development",
+                        nlText = "Website en model ontwikkeling",
+                        modifier = Modifier.color(SitePalette.light.primary)
+                            .margin(0.cssRem)
+                    )
+                },
+                description = {
+                    Column(Modifier.gap(0.5.cssRem)) {
+                        Row {
+                            MdiPhone(Modifier.padding(right = 0.25.cssRem))
+                            Text("+31 6 14910380")
+                        }
+
+                        Row {
+                            MdiMail(Modifier.padding(right = 0.25.cssRem))
+                            Text(ZenmoTeam.ATE.email)
+                        }
+                    }
+                },
+                actionText = {},
+                reversed = false,
+            )
+        }
+    }
+}
