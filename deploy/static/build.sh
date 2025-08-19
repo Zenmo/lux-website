99bedd6080fb3924b3e2ec17be53160133318533
Erik van Velzen <erik@evanv.nl>
2025-08-19 14:59:27 +0200

    Variables for test and prod deploy

--- deploy/static/build.sh
+++ deploy/static/build.sh
@@ -4,13 +4,16 @@ set -ex
 
 cd $(dirname "$0")
 
-source ../git-vars.sh
+#source ../git-vars.sh
 
 # It seems we can always specify GitHub Actions caching
 # and if it's not available it will fall back to local caching.
 docker buildx build \
     --file Dockerfile \
     --tag ghcr.io/zenmo/zenmo-website-static:$TAG \
+    --build-arg BACKEND_URL=$BACKEND_URL \
+    --build-arg ZENMO_DOMAIN=$ZENMO_DOMAIN \
+    --build-arg LUX_DOMAIN=$LUX_DOMAIN \
     --progress=plain \
     --cache-to type=gha,mode=max,scope=static \
     --cache-from type=gha,scope=static \
