90300ac6b6e7bfe68df7cb0116e9d3f587212bce
Kofi K. <kofivincent026@gmail.com>
2025-12-15 21:02:23 +0100

    add FAQs and "Work With Us" sections to Lux homepage

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/home/faqs/FaqsHeaderText.kt
@@ -0,0 +1,48 @@
+package com.zenmo.web.zenmo.domains.lux.sections.home.faqs
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiHelp
+import com.zenmo.web.zenmo.components.widgets.LangText
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.theme.SitePalette
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.P
+
+@Composable
+fun FaqsHeaderText() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            MdiHelp(
+                Modifier.Companion
+                    .fontSize(48.px)
+                    .color(SitePalette.Companion.light.primary).padding(right = 0.5.cssRem),
+                style = IconStyle.OUTLINED
+            )
+            HeaderText(
+                enText = "Frequently Asked Questions",
+                nlText = "Veelgestelde Vragen",
+                modifier = Modifier.Companion.margin(0.px)
+            )
+        }
+        P {
+            LangText(
+                en = "Find answers to common questions about LUX Energy Twin.",
+                nl = "Vind antwoorden op veelgestelde vragen over LUX Energy Twin."
+            )
+        }
+    }
+}
