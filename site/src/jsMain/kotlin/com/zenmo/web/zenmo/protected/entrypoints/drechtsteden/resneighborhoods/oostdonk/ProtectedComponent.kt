ba3182efcef7b0d4f364a190df5eb98b7b182db0
Erik van Velzen <erik@evanv.nl>
2026-01-22 09:53:43 +0100

    Swap Oostdonk and Over 't Spoor

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/protected/entrypoints/drechtsteden/resneighborhoods/oostdonk/ProtectedComponent.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/protected/entrypoints/drechtsteden/resneighborhoods/oostdonk/ProtectedComponent.kt
@@ -6,13 +6,13 @@ import energy.lux.site.shared.AccessPolicy
 import kotlin.uuid.Uuid
 
 @JsExport
-val accessPolicy = AccessPolicy.RoleBased("Oostdonk")
+val accessPolicy = AccessPolicy.RoleBased("Over 't Spoor")
 
 @JsExport
 @Composable
 fun ProtectedComponent() {
     AnyLogicEmbed(
-        modelId = Uuid.parse("612143ff-eedc-4f36-8606-13d4c4d710a7"),
+        modelId = Uuid.parse("3b4289ea-d2b8-4887-95bf-bf293fc34263"),
         apiKey = Uuid.parse("633268fe-104e-4dfd-a91b-9257c781ae69"),
     )
 }
