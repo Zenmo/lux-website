7f1694a32d7b74898b6610af5257b5d3957108c0
Kofi K. <kofivincent026@gmail.com>
2026-01-23 14:37:14 +0100

    refactor ProfileContactCard

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/ResponsiveRowStyle.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/ResponsiveRowStyle.kt
@@ -1,51 +1,51 @@
 package com.zenmo.web.zenmo.domains.lux.sections
 
 import com.varabyte.kobweb.compose.css.AlignItems
+import com.varabyte.kobweb.compose.css.functions.clamp
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import org.jetbrains.compose.web.css.DisplayStyle
-import org.jetbrains.compose.web.css.FlexDirection
-import org.jetbrains.compose.web.css.JustifyContent
-import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.*
 
 val ResponsiveRowStyle = CssStyle {
     base {
-        Modifier.Companion
+        Modifier
             .fillMaxWidth()
-            .display(DisplayStyle.Companion.Flex)
-            .alignItems(AlignItems.Companion.Center)
+            .display(DisplayStyle.Flex)
+            .alignItems(AlignItems.Center)
     }
 
     Breakpoint.ZERO {
-        Modifier.Companion
-            .flexDirection(FlexDirection.Companion.Column)
+        Modifier
+            .flexDirection(FlexDirection.Column)
             .gap(3.cssRem)
     }
 
     Breakpoint.SM {
-        Modifier.Companion
-            .flexDirection(FlexDirection.Companion.Column)
+        Modifier
+            .flexDirection(FlexDirection.Column)
             .gap(3.cssRem)
     }
     Breakpoint.MD {
-        Modifier.Companion
-            .flexDirection(FlexDirection.Companion.Row)
+        Modifier
+            .flexDirection(FlexDirection.Row)
             .gap(3.cssRem)
     }
 
     Breakpoint.LG {
-        Modifier.Companion
-            .flexDirection(FlexDirection.Companion.Row)
-            .justifyContent(JustifyContent.Companion.SpaceBetween)
+        Modifier
+            .flexDirection(FlexDirection.Row)
+            .justifyContent(JustifyContent.SpaceBetween)
             .gap(7.cssRem)
     }
 
     Breakpoint.XL {
-        Modifier.Companion
-            .flexDirection(FlexDirection.Companion.Row)
-            .justifyContent(JustifyContent.Companion.SpaceBetween)
+        Modifier
+            .flexDirection(FlexDirection.Row)
+            .justifyContent(JustifyContent.SpaceBetween)
             .gap(7.cssRem)
     }
 }
+
+fun Modifier.responsiveGap() = this.gap(clamp(2.cssRem, 5.vw, 5.cssRem))
