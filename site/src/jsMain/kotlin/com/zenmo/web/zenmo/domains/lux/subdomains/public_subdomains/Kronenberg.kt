fcab4a22ed66b5c149c4f31551e43b3ea3471ad7
kofi-k <kofivincent026@gmail.com>
2026-01-26 12:14:30 +0000

    refactor Kronenberg page (#395)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/public_subdomains/Kronenberg.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/public_subdomains/Kronenberg.kt
@@ -1,55 +1,57 @@
 package com.zenmo.web.zenmo.domains.lux.subdomains.public_subdomains
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.compose.ui.styleModifier
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.framework.annotations.DelicateApi
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import com.zenmo.web.zenmo.components.widgets.LangText
-import com.zenmo.web.zenmo.components.widgets.MediaContentLayout
-import com.zenmo.web.zenmo.components.widgets.SectionContainer
 import com.zenmo.web.zenmo.domains.lux.components.layout.LuxSubdomainPageLayout
 import com.zenmo.web.zenmo.domains.lux.core.model.subdomain.kronenberg
+import com.zenmo.web.zenmo.domains.lux.subdomains.components.SubdomainModelPage
 import com.zenmo.web.zenmo.domains.lux.subdomains.components.ZenmoModellerProfileCard
-import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
-import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
 import com.zenmo.web.zenmo.domains.zenmo.sections.team.ZenmoTeam
-import com.zenmo.web.zenmo.domains.zenmo.widgets.anylogic.AnyLogicEmbed
 import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.dom.Img
 import org.jetbrains.compose.web.dom.P
 
+@OptIn(DelicateApi::class)
 @Composable
 fun KronenbergIndex() {
     LuxSubdomainPageLayout(
         title = kronenberg.label.nl,
     ) {
-        SectionContainer(
-            modifier = Modifier
-                .gap(2.cssRem)
-                .margin(bottom = 2.cssRem),
-        ) {
-            Img(
-                src = "/lux/images/kronenberg/Energie-Kronenberg-4-1024x465.png",
-                alt = "EnergieKronenberg logo",
-                attrs = Modifier.margin(
-                    top = 1.cssRem, bottom = 2.cssRem
-                ).maxWidth(30.cssRem).toAttrs()
-            )
-
-            MediaContentLayout(
-                reversed = true,
-                imageUrl = "/lux/images/kronenberg/Skyline-Kronenberg-Jan-Janssen-scaled.jpg",
-                imageModifier = Modifier
-                    .height(30.cssRem)
-                    .styleModifier {
-                        // Issue https://github.com/Zenmo/lux-website/issues/355 to add this
-                        property("object-position", "50% bottom")
-                    },
-                description = {
-                    P {
+        val breakpoint = rememberBreakpoint()
+        SubdomainModelPage(
+            modelId = kronenberg.modelId,
+            introContent = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Img(
+                        src = "/lux/images/kronenberg/Energie-Kronenberg-4-1024x465.png",
+                        alt = "EnergieKronenberg logo",
+                        attrs = Modifier
+                            .margin(bottom = 2.cssRem)
+                            .maxWidth(30.cssRem).toAttrs()
+                    )
+                    P(
+                        Modifier.thenIf(
+                            breakpoint >= Breakpoint.MD, Modifier.width(60.percent)
+                        )
+                            .textAlign(TextAlign.Center)
+                            .toAttrs()
+                    ) {
                         LangText(
                             en = """
                                 Zenmo is developing a digital twin for the citizen collective EnergieKronenberg to
@@ -62,26 +64,8 @@ fun KronenbergIndex() {
                         )
                     }
                 }
-            )
-
-            AnyLogicEmbed(
-                modelId = kronenberg.modelId,
-            )
-
-            Column(
-                modifier = Modifier.fillMaxWidth().gap(1.cssRem),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                HeaderText(
-                    enText = "Contact and info", nlText = "Contact en info", modifier = Modifier.margin(0.cssRem)
-                )
-                SubHeaderText(
-                    enText = "Website and model development",
-                    nlText = "Website en model ontwikkeling",
-                    modifier = Modifier.margin(0.cssRem),
-                )
-                ZenmoModellerProfileCard(ZenmoTeam.NAUD_LOOMANS)
-            }
-        }
+            },
+            footerContent = { ZenmoModellerProfileCard(ZenmoTeam.NAUD_LOOMANS) }
+        )
     }
 }
