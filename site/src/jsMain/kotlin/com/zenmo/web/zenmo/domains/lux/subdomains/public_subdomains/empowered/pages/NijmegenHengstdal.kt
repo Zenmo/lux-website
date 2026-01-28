77a489d52997b343d1ab6d261130e24f2a54eed9
kofi-k <kofivincent026@gmail.com>
2026-01-28 11:23:05 +0000

    use SubdomainModelPage for NijmegenHengstdal page (#399)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/public_subdomains/empowered/pages/NijmegenHengstdal.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/public_subdomains/empowered/pages/NijmegenHengstdal.kt
@@ -1,32 +1,24 @@
 package com.zenmo.web.zenmo.domains.lux.subdomains.public_subdomains.empowered.pages
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.padding
-import com.zenmo.web.zenmo.components.widgets.SectionContainer
-import com.zenmo.web.zenmo.domains.lux.sections.LuxSectionContainerStyleVariant
+import com.zenmo.web.zenmo.domains.lux.subdomains.components.SubdomainModelPage
+import com.zenmo.web.zenmo.domains.lux.subdomains.components.ZenmoModellerProfileCard
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
-import com.zenmo.web.zenmo.domains.zenmo.widgets.anylogic.AnyLogicEmbed
-import org.jetbrains.compose.web.css.cssRem
+import com.zenmo.web.zenmo.domains.zenmo.sections.team.ZenmoTeam
 import kotlin.uuid.Uuid
 
 @Composable
 fun NijmegenHengstdal() {
-    SectionContainer(
-        variant = LuxSectionContainerStyleVariant
-    ) {
-        SubHeaderText(
-            enText = "Nijmegen Hengstdal",
-            nlText = "Nijmegen Hengstdal",
-        )
-    }
-
-    SectionContainer(
-        variant = LuxSectionContainerStyleVariant,
-        modifier = Modifier.padding(leftRight = 1.cssRem)
-    ) {
-        AnyLogicEmbed(
-            modelId = Uuid.parse("3801076e-44db-4b42-a27f-5be2b6c82a5c"),
-        )
-    }
+    SubdomainModelPage(
+        modelId = Uuid.parse("3801076e-44db-4b42-a27f-5be2b6c82a5c"),
+        introContent = {
+            SubHeaderText(
+                enText = "Nijmegen Hengstdal",
+                nlText = "Nijmegen Hengstdal",
+            )
+        },
+        footerContent = {
+            ZenmoModellerProfileCard(ZenmoTeam.NAUD_LOOMANS)
+        },
+    )
 }
