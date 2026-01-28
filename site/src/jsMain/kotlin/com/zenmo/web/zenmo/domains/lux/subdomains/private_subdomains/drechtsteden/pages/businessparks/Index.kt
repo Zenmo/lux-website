3ef9705bb58482c0fa9d15667375344615b13ed8
kofi-k <kofivincent026@gmail.com>
2026-01-28 09:04:35 +0000

    a simple BusinessParksPage (#397)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/pages/businessparks/Index.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/pages/businessparks/Index.kt
@@ -1,51 +1,37 @@
 package com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.css.AlignItems
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.graphics.lightened
-import com.varabyte.kobweb.compose.ui.modifiers.background
-import com.varabyte.kobweb.compose.ui.modifiers.borderLeft
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.padding
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.base
-import com.varabyte.kobweb.silk.style.toModifier
-import com.zenmo.web.zenmo.components.widgets.SectionContainer
-import com.zenmo.web.zenmo.domains.lux.sections.LuxSectionContainerStyleVariant
+import com.varabyte.kobweb.compose.ui.modifiers.alignItems
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.zenmo.web.zenmo.domains.lux.components.LuxSectionContainer
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.municipalities.drechtstedenMunicipalityModels
 import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.toTwinModelCardItems
 import com.zenmo.web.zenmo.domains.lux.widgets.TwinModelsGrid
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
-import com.zenmo.web.zenmo.theme.SitePalette
-import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.cssRem
 
-val BusinessParkHeaderStyle = CssStyle.base {
-    Modifier.fillMaxWidth()
-        .background(SitePalette.light.secondary.lightened(0.5f))
-        .borderLeft(.5.cssRem, LineStyle.Solid, SitePalette.light.secondary)
-        .padding(leftRight = 1.cssRem, topBottom = 0.5.cssRem)
-}
 
 @Composable
 fun BusinessParksPage() {
-    SectionContainer(
-        variant = LuxSectionContainerStyleVariant,
-        horizontalAlignment = Alignment.Start
+    LuxSectionContainer(
+        modifier = Modifier
+            .alignItems(AlignItems.FlexStart),
     ) {
         HeaderText(
             enText = "Business parks per municipality",
             nlText = "Bedrijventerreinen per gemeente",
-            modifier = _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks.BusinessParkHeaderStyle.toModifier()
+            modifier = Modifier.margin(0.cssRem)
         )
-        _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks.MunicipalitiesModels()
+        MunicipalitiesModels()
 
         HeaderText(
             enText = "Business parks",
             nlText = "Bedrijventerreinen",
-            modifier = _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks.BusinessParkHeaderStyle.toModifier()
+            modifier = Modifier.margin(0.cssRem)
         )
-        _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks.BusinessParksModels()
+        BusinessParksModels()
     }
 }
 
@@ -53,11 +39,11 @@ fun BusinessParksPage() {
 @Composable
 fun BusinessParksModels() =
     TwinModelsGrid(
-        models = _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks.drechtstedenBusinessParkModels.toTwinModelCardItems(),
+        models = drechtstedenBusinessParkModels.toTwinModelCardItems(),
     )
 
 @Composable
 fun MunicipalitiesModels() =
     TwinModelsGrid(
-        models = _root_ide_package_.com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.municipalities.drechtstedenMunicipalityModels.toTwinModelCardItems(),
+        models = drechtstedenMunicipalityModels.toTwinModelCardItems(),
     )
