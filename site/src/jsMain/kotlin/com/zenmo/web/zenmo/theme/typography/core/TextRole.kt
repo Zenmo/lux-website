8518227138ddc0fb208d1d2a7f4d6843dd7500b2
kofi-k <kofivincent026@gmail.com>
2026-02-02 14:04:53 +0000

    add typography system with text roles and styles (#405)

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/theme/typography/core/TextRole.kt
@@ -0,0 +1,8 @@
+package com.zenmo.web.zenmo.theme.typography.core
+
+enum class TextRole {
+    HEADER,
+    TITLE,
+    BODY,
+    LABEL,
+}
---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/theme/typography/fontstyles/TitleTextStyle.kt
@@ -0,0 +1,6 @@
+package com.zenmo.web.zenmo.theme.font
+
+import com.zenmo.web.zenmo.theme.typography.core.TextRole
+import com.zenmo.web.zenmo.theme.typography.fontstyles.typographyVariants
+
+val TitleTextStyle = TextStyle.typographyVariants(TextRole.TITLE)
