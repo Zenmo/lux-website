31027de4f78843b7aa7cdd7ce426f80f8b0761d2
Erik van Velzen <erik@evanv.nl>
2026-02-05 11:24:21 +0100

    Register catch-all page for all domains
    Fixes #425

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/pages/LuxRoutingComponent.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/pages/LuxRoutingComponent.kt
@@ -2,20 +2,19 @@ package com.zenmo.web.zenmo.domains.lux.pages
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.navigation.BasePath
-import com.varabyte.kobweb.navigation.Router
 import com.varabyte.kobweb.navigation.UpdateHistoryMode
 import com.varabyte.kobweb.navigation.remove
 import com.varabyte.kobweb.silk.defer.DeferringHost
-import com.zenmo.web.zenmo.components.widgets.CatchAllPage
 import com.zenmo.web.zenmo.core.models.asRoutes
 import com.zenmo.web.zenmo.domains.lux.components.layout.LuxEnergyLayout
+import com.zenmo.web.zenmo.domains.lux.core.createLuxRouter
 import com.zenmo.web.zenmo.domains.lux.sections.home.HomePage
 import com.zenmo.web.zenmo.domains.lux.sections.nav_header.luxNavMenu
 import kotlinx.browser.window
 
 @Composable
 fun LuxRoutingComponent() {
-    val router = Router()
+    val router = createLuxRouter()
     com.varabyte.kobweb.core.init.initKobweb(router) { ctx ->
         ctx.router.register("/") { LuxEnergyLayout { HomePage() } }
 
@@ -27,7 +26,6 @@ fun LuxRoutingComponent() {
         if (window.location.host != "lux.energy") {
             ctx.router.register("/component-demo") { ComponentDemoPage() }
         }
-        ctx.router.register("/{...catch-all}") { CatchAllPage() }
         ctx.router.register("/book-demo") { BookADemoPage() }
     }
 
