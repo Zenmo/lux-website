3b6f5213f38219403d4bca9bfb870a94d4d99f4f
kofi-k <kofivincent026@gmail.com>
2026-02-18 13:25:50 +0000

    business-parks-main-page-text (#442)
    * text for woonwijken page and models
    
    * business parks main page text

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/components/ResidentialAreaModelPageText.kt
@@ -0,0 +1,116 @@
+package com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.components
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.transform
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiBattery0Bar
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiElectricCar
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiHeatPump
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiLocalFireDepartment
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.toModifier
+import com.zenmo.web.zenmo.components.widgets.LangText
+import com.zenmo.web.zenmo.core.services.localization.LocalizedText
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.deg
+import org.jetbrains.compose.web.dom.Br
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.P
+
+
+@Composable
+fun ResidentialAreaModelPageText() {
+    Column {
+        Div {
+            P {
+                LangText(
+                    en = """
+                    With the energy twin for residential neighborhoods and districts, municipalities can examine their 
+                    energy plans and transition visions in detail. The energy twin integrates sustainable mobility, 
+                    sustainable heating, and electrification. The neighborhood is simulated in detail with 
+                    sustainability options per household, placement of public charging stations, and potential heating 
+                    networks.
+                """.trimIndent(),
+                    nl = """
+                    Met de energy twin voor woonwijken en buurten kunnen gemeenten hun energieplannen en 
+                    transitievisies in detail doorlichten. De energy twin integreert duurzame mobiliteit, duurzame 
+                    warmte en elektrificatie. De wijk wordt gedetailleerd gesimuleerd met verduurzamingsopties per 
+                    woning, plaatsing van openbare laadpalen en mogelijke warmtenetten. 
+                """.trimIndent()
+                )
+            }
+            P {
+                LangText(
+                    en = """
+                    We also explore unconventional solutions that provide actionable options for neighborhoods facing 
+                    grid congestion but lacking heating network opportunities.
+                """.trimIndent(),
+                    nl = """
+                    Ook verkennen we onconventionele oplossingen die handelingsperspectief bieden voor wijken met 
+                    netcongestie, maar zonder mogelijkheid voor warmtenetten.
+                """.trimIndent()
+                )
+                Br { }
+                LangText(
+                    en = """
+                        These include neighborhood batteries and smart control of (hybrid) heat pumps, home batteries, 
+                        and electric vehicles.
+                    """.trimIndent(),
+                    nl = """
+                        Denk hierbij aan buurtbatterijen en slimme aansturing van (hybride) warmtepompen, 
+                        thuisbatterijen en elektrische voertuigen.
+                    """.trimIndent()
+                )
+            }
+        }
+        UnconventionalSolutions()
+    }
+}
+
+val BatteryRotationStyle = CssStyle.base {
+    Modifier.transform {
+        rotate(90.deg)
+    }
+}
+
+@Composable
+private fun UnconventionalSolutions() {
+    SimpleGrid(
+        numColumns = numColumns(base = 1, sm = 1, md = 2, lg = 4, xl = 4),
+        modifier = Modifier.gap(1.5.cssRem)
+    ) {
+        FeatureItem(
+            text = LocalizedText(
+                en = "Neighborhood and home batteries",
+                nl = "Buurt -en thuisbatterijen"
+            ),
+            icon = { MdiBattery0Bar(BatteryRotationStyle.toModifier()) },
+        )
+        FeatureItem(
+            text = LocalizedText(
+                en = "smart control of (hybrid) heat pumps",
+                nl = "slimme aansturing van (hybride) warmtepompen"
+            ),
+            icon = { MdiHeatPump() }
+        )
+        FeatureItem(
+            text = LocalizedText(
+                en = "District heating",
+                nl = "Warmtenetten"
+            ),
+            icon = { MdiLocalFireDepartment() }
+        )
+        FeatureItem(
+            text = LocalizedText(
+                en = "Electric vehicles with smart charging and V2G",
+                nl = "Elektrische voertuigen met slim laden en V2G"
+            ),
+            icon = { MdiElectricCar() }
+        )
+    }
+}
--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/pages/businessparks/Index.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/pages/businessparks/Index.kt
@@ -1,34 +1,51 @@
 package com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.businessparks
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.AlignItems
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.alignItems
-import com.zenmo.web.zenmo.domains.lux.components.LuxSectionContainer
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.zenmo.web.zenmo.domains.lux.subdomains.components.SubdomainModelPage
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.components.BusinessParksText
 import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.pages.municipalities.drechtstedenMunicipalityModels
 import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.toTwinModelCardItems
 import com.zenmo.web.zenmo.domains.lux.widgets.TwinModelsGrid
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import org.jetbrains.compose.web.css.cssRem
+import kotlin.uuid.Uuid
 
 
 @Composable
 fun BusinessParksPage() {
-    LuxSectionContainer(
-        modifier = Modifier
-            .alignItems(AlignItems.FlexStart),
-    ) {
-        HeaderText(
-            enText = "Business parks per municipality",
-            nlText = "Bedrijventerreinen per gemeente",
-        )
-        MunicipalitiesModels()
+    SubdomainModelPage(
+        modelId = Uuid.NIL,
+        introContent = {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .gap(1.5.cssRem)
+            ) {
+                HeaderText(
+                    enText = "Energy hubs",
+                    nlText = "Energy hubs",
+                )
+                BusinessParksText()
+            }
+        },
+        anylogicRender = {},
+        footerContent = {
+            HeaderText(
+                enText = "Business parks per municipality",
+                nlText = "Bedrijventerreinen per gemeente",
+            )
+            MunicipalitiesModels()
 
-        HeaderText(
-            enText = "Business parks",
-            nlText = "Bedrijventerreinen",
-        )
-        BusinessParksModels()
-    }
+            HeaderText(
+                enText = "Business parks",
+                nlText = "Bedrijventerreinen",
+            )
+            BusinessParksModels()
+        }
+    )
 }
 
 
