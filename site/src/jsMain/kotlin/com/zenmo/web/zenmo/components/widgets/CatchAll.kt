d30f6c22b59b2f9a8ef4b9a41974cb68aef07734
kofi-k <kofivincent026@gmail.com>
2026-02-18 10:21:54 +0000

    a less ugly CatchAll page (#437)

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/widgets/CatchAll.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/components/widgets/CatchAll.kt
@@ -2,37 +2,56 @@ package com.zenmo.web.zenmo.components.widgets
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.TextAlign
-import com.varabyte.kobweb.compose.css.TextDecorationLine
-import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.css.TextTransform
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.gap
 import com.varabyte.kobweb.compose.ui.modifiers.textAlign
-import com.varabyte.kobweb.compose.ui.modifiers.textDecorationLine
-import com.varabyte.kobweb.silk.components.icons.mdi.MdiArrowBack
-import com.varabyte.kobweb.silk.components.navigation.Link
-import com.zenmo.web.zenmo.domains.lux.sections.LuxSectionContainerStyleVariant
+import com.varabyte.kobweb.compose.ui.modifiers.textTransform
+import com.zenmo.web.zenmo.domains.lux.components.LuxSectionContainer
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
-import com.zenmo.web.zenmo.domains.zenmo.widgets.button.IconButton
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.SubHeaderText
+import com.zenmo.web.zenmo.domains.zenmo.widgets.button.PrimaryButton
+import com.zenmo.web.zenmo.theme.SitePalette
+import com.zenmo.web.zenmo.utils.PublicRes
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.dom.P
 
 @Composable
 fun CatchAllPage() {
-    SectionContainer(
-        modifier = Modifier.textAlign(TextAlign.Center),
-        verticalArrangement = Arrangement.Center,
-        variant = LuxSectionContainerStyleVariant
-    ) {
+    LuxSectionContainer {
         HeaderText(
-            enText = "This page does not exist.",
-            nlText = "Deze pagina bestaat niet."
+            enText = "404",
+            nlText = "404",
+            textColor = SitePalette.light.primary
         )
-        Link(
-            path = "/",
-            modifier = Modifier.textDecorationLine(TextDecorationLine.None)
+
+        Column(
+            Modifier
+                .gap(1.cssRem)
+                .textAlign(TextAlign.Center)
         ) {
-            IconButton(onClick = { }, modifier = Modifier.size(3.cssRem)) {
-                MdiArrowBack()
+            SubHeaderText(
+                enText = "This page does not exist.",
+                nlText = "Deze pagina bestaat niet.",
+                modifier = Modifier
+                    .fontFamily(PublicRes.FontFamilies.HOLON_BLOCK)
+                    .textTransform(TextTransform.Uppercase)
+            )
+            P {
+                LangText(
+                    en = "The page you're looking for seems to have wandered off the grid.",
+                    nl = "De pagina die je zoekt lijkt van de kaart verdwenen te zijn."
+                )
             }
         }
+
+        PrimaryButton(
+            enText = "GO BACK",
+            nlText = "GA TERUG",
+            onClick = { window.history.back() }
+        )
     }
 }
