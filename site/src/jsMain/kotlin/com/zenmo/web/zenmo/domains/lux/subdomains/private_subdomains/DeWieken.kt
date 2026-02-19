041fcae999d0195e2bb5e52fa7943d388bbbc81e
Erik van Velzen <erik@evanv.nl>
2026-02-19 14:16:08 +0100

    De Wieken text
    Forgot to add it. Is copied from the old site.

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/DeWieken.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/DeWieken.kt
@@ -18,6 +18,24 @@ fun DeWieken() {
         PrivateTwinModelPage(
             entryPoint = deWieken.entryPoint,
             imageUrl = deWieken.imageUrl,
+            introContent = {
+                P {
+                    LangText(
+                        nl = """
+                            Bekijk hieronder  de energy twin voor de verduurzaming van bedrijventerrein De Wieken in Amersfoort. 
+                            De bedrijven op dit bedrijventerrein onderzoeken of ze samen energie kunnen delen 
+                            en investeren in duurzame technieken. 
+                            De energy twin helpt bij het ontwerpen van deze duurzame systemen en het maken van de juiste beslissingen.
+                        """,
+                        en = """
+                            Below is the energy twin for making the De Wieken business park in Amersfoort more sustainable. 
+                            The companies at this business park are investigating whether they can share energy 
+                            and invest in sustainable technologies. 
+                            The energy twin helps design these sustainable systems and make the right decisions.
+                        """
+                    )
+                }
+            },
             footerContent = {
                 ZenmoModellerProfileCard(ZenmoTeam.LUC)
             }
