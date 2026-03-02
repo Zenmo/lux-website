7d4b29bf85726b489da7d81eebe5303b39c70ecc
Erik van Velzen <erik@evanv.nl>
2026-03-02 17:27:40 +0100

    Update kotlin to 2.3.10
    Also Kobweb, Compose and kotlinx-serialization.
    
    I don't notice any regressions.

--- prerender/settings.gradle.kts
+++ prerender/settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    kotlin("jvm") version "2.2.21" apply false
+    kotlin("jvm") version "2.3.10" apply false
 }
 
 rootProject.name = "prerender"
