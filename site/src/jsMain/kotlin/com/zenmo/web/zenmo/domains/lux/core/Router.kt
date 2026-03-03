b34e93584ca3556ec9d1ebab4755bf19cd6c1ead
Erik van Velzen <erik@evanv.nl>
2026-03-03 19:40:35 +0100

    Add hreflang elements to every page for crawlers

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/core/Router.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/core/Router.kt
@@ -2,9 +2,13 @@ package com.zenmo.web.zenmo.domains.lux.core
 
 import com.varabyte.kobweb.navigation.Router
 import com.zenmo.web.zenmo.components.widgets.CatchAllPage
+import com.zenmo.web.zenmo.core.services.localization.addHreflangInterceptor
 
 fun createLuxRouter(): Router = Router()
-    .also { it.registerLuxCatchAllPage() }
+    .also {
+        it.registerLuxCatchAllPage()
+        it.addHreflangInterceptor()
+    }
 
 fun Router.registerLuxCatchAllPage() {
     this.register("/{...catch-all}") { CatchAllPage() }
