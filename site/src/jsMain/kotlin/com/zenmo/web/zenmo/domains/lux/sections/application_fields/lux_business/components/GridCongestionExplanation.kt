dbf1f898519e637dceddb898a7fd7b7783201157
Kofi K. <kofivincent026@gmail.com>
2026-03-05 05:25:41 +0100

    text for LUX Business

---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/application_fields/lux_business/components/GridCongestionExplanation.kt
@@ -0,0 +1,137 @@
+package com.zenmo.web.zenmo.domains.lux.sections.application_fields.lux_business.components
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.functions.clamp
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiBattery0Bar
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiElectricBolt
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiSolarPower
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.toModifier
+import com.zenmo.web.zenmo.components.widgets.LangText
+import com.zenmo.web.zenmo.core.services.localization.LocalizedText
+import com.zenmo.web.zenmo.domains.lux.components.LuxSectionContainer
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.components.BatteryRotationStyle
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.theme.LuxSpecificColorHues
+import com.zenmo.web.zenmo.theme.SitePalette
+import com.zenmo.web.zenmo.theme.styles.luxBorderRadius
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vw
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Span
+
+
+@Composable
+fun GridCongestionExplanation() {
+    LuxSectionContainer(
+        Modifier
+            .background(LuxSpecificColorHues().luxBlackRussian)
+            .color(Colors.White)
+            .textAlign(TextAlign.Center)
+            .gap(1.cssRem),
+    ) {
+        HeaderText(
+            enText = "There are other ways!",
+            nlText = "Het kan ook anders!"
+        )
+        Column(
+            modifier = Modifier
+                .width(clamp(350.px, 60.vw, 700.px))
+                .gap(1.cssRem),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            P {
+                LangText(
+                    en = """
+                        For example, a business can grow without increasing its grid connection by installing a battery 
+                        or solar panels; they can also try to optimize their consumption profile over the day. But is 
+                        this really going to take care of your problem? What exactly is the impact of this on your 
+                        energy consumption? 
+                    """.trimIndent(),
+                    nl = """
+                        Zo kan een bedrijf bijvoorbeeld groeien zonder zijn netaansluiting te verzwaren door een 
+                        batterij te plaatsen, of zonnepanelen; vaak is er ook de mogelijkheid om het consumptieprofiel 
+                        te optimaliseren. Maar is jouw bedrijf hiermee echt uit de problemen? Wat is nu precies het 
+                        effect hiervan op je energiegebruik? 
+                    """.trimIndent()
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .gap(12.px),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                IconTextItem(
+                    text = LocalizedText(
+                        en = "Battery Storage",
+                        nl = "Batterij Opslag"
+                    ),
+                    icon = { MdiBattery0Bar(BatteryRotationStyle.toModifier()) },
+                )
+
+                IconTextItem(
+                    text = LocalizedText(
+                        en = "Solar Panels",
+                        nl = "Zonnepanelen"
+                    ),
+                    icon = { MdiSolarPower() },
+                )
+
+                IconTextItem(
+                    text = LocalizedText(
+                        en = "Smart Optimization",
+                        nl = "Slimme Optimalisatie"
+                    ),
+                    icon = { MdiElectricBolt() },
+                )
+            }
+        }
+    }
+}
+
+val IconTextItemStyle = CssStyle.base {
+    Modifier
+        .fillMaxSize()
+        .padding(12.px)
+        .gap(8.px)
+        .background(Colors.White.copyf(alpha = 0.1f))
+        .luxBorderRadius()
+}
+
+@Composable
+private fun IconTextItem(
+    text: LocalizedText,
+    icon: @Composable () -> Unit,
+) {
+    Column(
+        IconTextItemStyle.toModifier(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Span(
+            Modifier
+                .color(SitePalette.light.secondary)
+                .toAttrs(),
+        ) {
+            icon()
+        }
+        LangText(
+            en = text.en,
+            nl = text.nl,
+        )
+    }
+}
---
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/application_fields/lux_business/components/LuxBusinessHero.kt
@@ -0,0 +1,123 @@
+package com.zenmo.web.zenmo.domains.lux.sections.application_fields.lux_business.components
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiWarning
+import com.varabyte.kobweb.silk.style.toModifier
+import com.zenmo.web.zenmo.components.widgets.LangText
+import com.zenmo.web.zenmo.domains.lux.components.LuxSectionContainer
+import com.zenmo.web.zenmo.domains.lux.sections.ResponsiveFlexStyle
+import com.zenmo.web.zenmo.domains.lux.sections.application_fields.LuxApplicationArea
+import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.theme.LuxSpecificColorHues
+import com.zenmo.web.zenmo.theme.SitePalette
+import org.jetbrains.compose.web.css.AlignItems
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.B
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.P
+
+@Composable
+fun LuxBusinessHero() {
+    LuxSectionContainer(
+        modifier = Modifier.alignItems(AlignItems.FlexStart)
+    ) {
+        HeaderText(
+            enText = LuxApplicationArea.LUX_BUSINESS.label.en,
+            nlText = LuxApplicationArea.LUX_BUSINESS.label.nl,
+        )
+        Column {
+            Row(
+                Modifier.gap(8.px)
+                    .color(LuxSpecificColorHues().luxRed),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                MdiWarning()
+                LangText(
+                    en = "The Problem",
+                    nl = "Het Probleem"
+                )
+            }
+            Div(
+                ResponsiveFlexStyle.toModifier()
+                    .alignItems(AlignItems.FlexStart)
+                    .toAttrs()
+            ) {
+                Div(
+                    Modifier.fillMaxWidth()
+                        .toAttrs()
+                ) {
+                    P {
+                        LangText(
+                            en = """
+                            Businesses are rapidly electrifying, but they are also increasingly being told by grid 
+                            operators that they won't be able to get the power they need in the short term because 
+                            there is insufficient transmission capacity.
+                        """.trimIndent(),
+                            nl = """
+                            Bedrijven elektrificeren in hoog tempo maar tegelijkertijd krijgen ze steeds vaker van de 
+                            netbeheerder te horen dat ze op korte termijn niet de stroom kunnen krijgen die ze nodig 
+                            hebben, omdat er onvoldoende transportcapaciteit beschikbaar is.
+                        """.trimIndent()
+                        )
+                    }
+                    P {
+                        LangText(
+                            en = "This problem is called ",
+                            nl = "Dit probleem noemen we "
+                        )
+                        B {
+                            LangText(
+                                en = "grid congestion.",
+                                nl = "netcongestie."
+                            )
+                        }
+                    }
+                }
+
+                Div(
+                    Modifier.fillMaxWidth()
+                        .toAttrs()
+                ) {
+                    P {
+                        LangText(
+                            en = """
+                            As a result, businesses are often unable to expand, electrify, or settle in the designated 
+                            areas. In some cases, this leads to companies generating their own electricity with diesel 
+                            generators.
+                        """.trimIndent(),
+                            nl = """
+                            In sommige gevallen leidt dit er toe dat bedrijven stroom gaan opwekken met 
+                            dieselaggregaten.  
+                        """.trimIndent()
+                        )
+                    }
+                    P(
+                        Modifier.margin(0.px)
+                            .padding(left = 24.px)
+                            .fontWeight(FontWeight.Bold)
+                            .borderLeft(4.px, LineStyle.Solid, SitePalette.light.secondary)
+                            .toAttrs()
+                    ) {
+                        LangText(
+                            en = """
+                            An expensive "solution" that runs counter to sustainability.
+                        """.trimIndent(),
+                            nl = """
+                            Dit is een dure “oplossing” die haaks staat op de verduurzaming waar we in Nederland naar 
+                            streven. 
+                        """.trimIndent()
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
