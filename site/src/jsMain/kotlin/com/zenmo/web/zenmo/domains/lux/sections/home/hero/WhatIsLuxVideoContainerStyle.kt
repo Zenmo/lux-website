a62ccdc518e3f3a67f17b17f5c88d4817d080793
Kofi K. <kofivincent026@gmail.com>
2026-03-09 21:33:40 +0100

    a less 'grim' and more inviting landing page
    also closes #477

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/home/hero/WhatIsLuxVideoContainerStyle.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/sections/home/hero/WhatIsLuxVideoContainerStyle.kt
@@ -2,27 +2,18 @@ package com.zenmo.web.zenmo.domains.lux.sections.home.hero
 
 import com.varabyte.kobweb.compose.css.Overflow
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.aspectRatio
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.modifiers.overflow
 import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.base
 import com.zenmo.web.zenmo.theme.styles.LuxCornerRadius
 import com.zenmo.web.zenmo.theme.styles.luxBorderRadius
-import org.jetbrains.compose.web.css.px
 
-val WhatIsLuxVideoContainerStyle = CssStyle {
-    base {
-        Modifier.Companion
-            .fillMaxWidth()
-            .height(225.px)
-            .luxBorderRadius(LuxCornerRadius.lg)
-            .overflow(Overflow.Clip)
-    }
-    Breakpoint.LG {
-        Modifier.height(250.px)
-    }
-    Breakpoint.XL {
-        Modifier.height(300.px)
-    }
+val WhatIsLuxVideoContainerStyle = CssStyle.base {
+    Modifier.Companion
+        .fillMaxWidth()
+        .luxBorderRadius(LuxCornerRadius.lg)
+        .overflow(Overflow.Clip)
+        .aspectRatio(16f / 9f)
 }
