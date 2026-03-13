0a837ee5337c65359a1f2c6ea35f87db1d925ebe
Kofi K. <kofivincent026@gmail.com>
2026-03-13 15:21:04 +0100

    updates DrechtstedenTwinModel structure to have public models
    - also add public Land Van Valk neighborhood model

--- site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/components/DrechtstedenTwinModelPage.kt
+++ site/src/jsMain/kotlin/com/zenmo/web/zenmo/domains/lux/subdomains/private_subdomains/drechtsteden/components/DrechtstedenTwinModelPage.kt
@@ -5,20 +5,25 @@ import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.gap
-import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.components.PrivateTwinModelPage
+import com.zenmo.web.zenmo.domains.lux.subdomains.components.SubdomainModelPage
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.components.ModelWrapper
 import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.DrechtstedenTwinModel
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.DrechtstedenTwinModelBase
+import com.zenmo.web.zenmo.domains.lux.subdomains.private_subdomains.drechtsteden.PublicDrechtstedenTwinModel
 import com.zenmo.web.zenmo.domains.lux.widgets.headings.HeaderText
+import com.zenmo.web.zenmo.domains.zenmo.widgets.anylogic.AnyLogicEmbed
+import com.zenmo.web.zenmo.domains.zenmo.widgets.anylogic.anyLogicPublicApiKey
 import org.jetbrains.compose.web.css.cssRem
+import kotlin.uuid.Uuid
 
 @Composable
 fun DrechtstedenTwinModelPage(
-    twin: DrechtstedenTwinModel,
+    twin: DrechtstedenTwinModelBase,
     introContent: @Composable () -> Unit = {},
     mediaContent: @Composable () -> Unit = {},
     footerContent: @Composable () -> Unit,
-) = PrivateTwinModelPage(
-    entryPoint = twin.entryPoint,
-    imageUrl = twin.imageUrl,
+) = SubdomainModelPage(
+    modelId = Uuid.NIL,
     introContent = {
         Column(
             Modifier.fillMaxWidth()
@@ -29,6 +34,23 @@ fun DrechtstedenTwinModelPage(
                 nlText = twin.label.nl,
             )
             introContent()
+        }
+    },
+    anylogicRender = {
+        when (twin) {
+            is PublicDrechtstedenTwinModel -> {
+                AnyLogicEmbed(
+                    modelId = twin.modelId,
+                    apiKey = anyLogicPublicApiKey,
+                )
+            }
+
+            is DrechtstedenTwinModel -> {
+                ModelWrapper(
+                    imgUrl = twin.imageUrl,
+                    entryPoint = twin.entryPoint,
+                )
+            }
         }
     },
     mediaContent = mediaContent,
