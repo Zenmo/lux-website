195d4f10ba5d328c24039ffc05990412093dfa30
Erik van Velzen <erik@evanv.nl>
2026-04-13 13:18:47 +0200

    delete accidentally committed files

--- site/src/jsMain/kotlin/protected/entrypoints/drechtsteden/DrechtstedenApiKey.kt
+++
@@ -1,8 +0,0 @@
-package energy.lux.frontend.protected.entrypoints.drechtsteden
-
-import kotlin.uuid.Uuid
-
-/**
- * AnyLogic Private Cloud API Key
- */
-val drechtstedenApiKey = Uuid.parse("30e795bf-7617-4776-94b2-d34fabe18ccc")
--- site/src/jsMain/kotlin/protected/entrypoints/drechtsteden/businessparks/NieuweWegApiKey.kt
+++
@@ -1,3 +0,0 @@
-import kotlin.uuid.Uuid
-
-val nieuweWegApiKey = Uuid.parse("5abb9d5e-afd5-44c2-84e2-205c8deec86f")
