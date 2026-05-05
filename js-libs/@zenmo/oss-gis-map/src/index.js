f65ab4c826822c22af6962745674c201bd2e1b51
Erik van Velzen <erik@evanv.nl>
2026-05-05 16:23:07 +0200

    Oss: add project areas to map

--- js-libs/@zenmo/oss-gis-map/src/index.js
+++ js-libs/@zenmo/oss-gis-map/src/index.js
@@ -1,5 +1,6 @@
 import {config, Map} from "@maptiler/sdk"
 import {loadStyles} from "./loadStyles.js"
+import {areas, enrichAreasWithGeometry} from "./areas.js"
 
 const OSS_CENTER = [5.53289, 51.76151] // [longitude, latitude]
 const OSS_MAP_STYLE_ID = "019df291-09a3-786a-8db0-8690f20fc678"
@@ -14,7 +15,7 @@ config.apiKey = API_KEY;
  */
 export async function showOssMap(element) {
     loadStyles()
-    return new Promise((resolve, reject) => {
+    const mapPromise = new Promise((resolve, reject) => {
         const map = new Map({
             container: element,
             style: `https://api.maptiler.com/maps/${OSS_MAP_STYLE_ID}/style.json?key=${API_KEY}`,
@@ -25,5 +26,50 @@ export async function showOssMap(element) {
         map.on("load", () => resolve(map))
         map.on("error", (e) => reject(e.error ?? e))
     })
+
+    const [map, enriched] = await Promise.all([
+        mapPromise,
+        enrichAreasWithGeometry(areas)
+    ])
+
+    map.addSource("areas", {
+        type: "geojson",
+        data: {
+            type: "FeatureCollection",
+            features: enriched
+                .map(a => ({
+                    ...a.geojson,
+                    properties: {
+                        ...a.geojson.properties,
+                        name: a.name,
+                        url: a.url
+                    }
+                }))
+        }
+    })
+
+    const luxTeal = "rgb(0, 126, 132)"
+
+    map.addLayer({
+        id: "areas-fill",
+        type: "fill",
+        source: "areas",
+        paint: {
+            "fill-color": luxTeal,
+            "fill-opacity": 0.4
+        }
+    })
+
+    map.addLayer({
+        id: "areas-outline",
+        type: "line",
+        source: "areas",
+        paint: {
+            "line-color": luxTeal,
+            "line-width": 2
+        }
+    })
+
+    return map
 }
 
