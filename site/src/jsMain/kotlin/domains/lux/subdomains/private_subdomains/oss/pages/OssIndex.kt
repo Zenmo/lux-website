3638af876bdad0fa4bf3794e19adb41c15bcd651
Erik van Velzen <erik@evanv.nl>
2026-05-06 03:17:07 +0200

    Oss: layout improvements
    Also fixes swapped model ids.
    
    Closes #603

--- site/src/jsMain/kotlin/domains/lux/subdomains/private_subdomains/oss/pages/OssIndex.kt
+++ site/src/jsMain/kotlin/domains/lux/subdomains/private_subdomains/oss/pages/OssIndex.kt
@@ -1,41 +1,49 @@
 package energy.lux.frontend.domains.lux.subdomains.private_subdomains.oss.pages
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import energy.lux.frontend.components.widgets.LangText
+import energy.lux.frontend.components.widgets.SectionContainer
+import energy.lux.frontend.domains.lux.components.LuxSectionContainer
 import energy.lux.frontend.domains.lux.core.toTwinModelCardItem
-import energy.lux.frontend.domains.lux.subdomains.components.SubdomainModelPage
 import energy.lux.frontend.domains.lux.subdomains.private_subdomains.oss.OssGisMap
 import energy.lux.frontend.domains.lux.subdomains.private_subdomains.oss.ossTwinModels
 import energy.lux.frontend.domains.lux.widgets.TwinModelsGrid
 import energy.lux.frontend.domains.lux.widgets.headings.HeaderText
-import kotlin.uuid.Uuid
+import energy.lux.frontend.theme.SitePalette
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.dom.P
 
 
 @Composable
 fun OssIndex() {
-    SubdomainModelPage(
-        modelId = Uuid.NIL,
-        anylogicRender = {},
-        introContent = {
-            HeaderText(
-                enText = "This is the Oss Project",
-                nlText = "Dit is het Oss Project",
+    SectionContainer(
+        modifier = Modifier.padding(top = 2.cssRem, bottom = 1.cssRem),
+    ) {
+        HeaderText(
+            enText = "Oss Energy Twins",
+            nlText = "Oss Energy Twins",
+        )
+
+        P {
+            LangText(
+                en = "Explore the energy dynamics of this industrial center.",
+                nl = "Verken de energiedynamieken van deze industriestad.",
             )
-        },
-        mediaContent = {
-            OssGisMap()
-        },
-        extraContent = {
-        },
-        footerContent = {
-            HeaderText(
-                enText = "Explore the Models",
-                nlText = "Verken de Modellen",
-            )
-            TwinModelsGrid(
-                models = ossTwinModels.map {
-                    it.toTwinModelCardItem()
-                }
-            )
-        },
-    )
+        }
+    }
+
+    OssGisMap()
+
+    LuxSectionContainer(
+        modifier = Modifier.backgroundColor(SitePalette.light.overlay)
+    ) {
+        TwinModelsGrid(
+            models = ossTwinModels.map {
+                it.toTwinModelCardItem()
+            }
+        )
+    }
 }
