35640926e315782b5819821cd66402e2f46fe1ec
kofi-k <kofivincent026@gmail.com>
2026-05-19 19:31:29 +0000

    add new residential neighborhood models for Centrum Hardinxveld-Giessendam, Schildersbuurt Zwijndrecht, and Kruiswiel Hendrik-Ido-Ambacht (#621)

---
+++ site/src/jsMain/kotlin/domains/lux/subdomains/private_subdomains/drechtsteden/pages/residential_neighborhoods/CentrumHardinxveldGiessendam.kt
@@ -0,0 +1,18 @@
+package energy.lux.frontend.domains.lux.subdomains.private_subdomains.drechtsteden.pages.residential_neighborhoods
+
+import androidx.compose.runtime.Composable
+import energy.lux.frontend.domains.lux.subdomains.components.ZenmoModellerProfileCard
+import energy.lux.frontend.domains.lux.subdomains.private_subdomains.drechtsteden.components.DrechtstedenTwinModelPage
+import energy.lux.frontend.domains.lux.subdomains.private_subdomains.drechtsteden.components.ResidentialAreaModelPageText
+import energy.lux.frontend.domains.zenmo.sections.team.ZenmoTeam
+
+@Composable
+fun CentrumHardinxveldGiessendam() {
+    DrechtstedenTwinModelPage(
+        twin = centrumHardinxveldGiessendam,
+        introContent = { ResidentialAreaModelPageText() },
+        footerContent = {
+            ZenmoModellerProfileCard(ZenmoTeam.NAUD_LOOMANS)
+        }
+    )
+}
