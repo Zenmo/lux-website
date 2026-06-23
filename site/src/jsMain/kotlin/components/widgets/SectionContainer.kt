d5aabb01f079178b6cc5732f46f8df54222b05d9
kofi-k <kofivincent026@gmail.com>
2026-06-23 23:56:58 +0000

    apply fluid spacing to SectionContainer and SubdomainModelPage

--- site/src/jsMain/kotlin/components/widgets/SectionContainer.kt
+++ site/src/jsMain/kotlin/components/widgets/SectionContainer.kt
@@ -6,16 +6,14 @@ import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.height
-import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.style.ComponentKind
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.CssStyleVariant
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toModifier
+import energy.lux.frontend.theme.SiteFluidSpacing
+import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.keywords.auto
-import org.jetbrains.compose.web.css.px
 
 sealed interface SectionComponentKind : ComponentKind
 
@@ -25,21 +23,12 @@ val SectionContainerStyle = CssStyle<SectionComponentKind> {
         Modifier
             .fillMaxWidth()
             .height(auto)
-    }
-    Breakpoint.ZERO {
-        Modifier.padding(leftRight = 16.px)
-    }
-    Breakpoint.SM {
-        Modifier.padding(leftRight = 28.px)
-    }
-    Breakpoint.MD {
-        Modifier.padding(leftRight = 48.px)
-    }
-    Breakpoint.LG {
-        Modifier.padding(leftRight = 94.px)
-    }
-    Breakpoint.XL {
-        Modifier.padding(leftRight = 134.px)
+            .display(DisplayStyle.Flex)
+            .padding(
+                leftRight = SiteFluidSpacing.current.horizontalPadding,
+                topBottom = SiteFluidSpacing.current.verticalPadding
+            )
+            .gap(SiteFluidSpacing.current.gap)
     }
 }
 
