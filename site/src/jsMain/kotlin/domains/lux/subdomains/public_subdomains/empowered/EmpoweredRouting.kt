a41296f2715b98de162a976b241cb31092dabd25
kofi-k <kofivincent026@gmail.com>
2026-06-26 05:25:08 -0700

    modularize Empowered subdomain components and models (#654)

--- site/src/jsMain/kotlin/domains/lux/subdomains/public_subdomains/empowered/EmpoweredRouting.kt
+++ site/src/jsMain/kotlin/domains/lux/subdomains/public_subdomains/empowered/EmpoweredRouting.kt
@@ -6,15 +6,9 @@ import com.varabyte.kobweb.navigation.BasePath
 import com.varabyte.kobweb.navigation.UpdateHistoryMode
 import com.varabyte.kobweb.navigation.remove
 import com.varabyte.kobweb.silk.defer.DeferringHost
-import energy.lux.frontend.core.models.MenuItem
-import energy.lux.frontend.core.models.RoutedMenuItem
 import energy.lux.frontend.core.models.asRoutes
-import energy.lux.frontend.core.services.localization.LocalizedText
 import energy.lux.frontend.domains.lux.core.createLuxRouter
 import energy.lux.frontend.domains.lux.pages.registerRoutesOfMenu
-import energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.pages.Bronckhorst
-import energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.pages.NijmegenHengstdal
-import energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.pages.empowered.EmpoweredIndex
 import kotlinx.browser.window
 
 @Composable
@@ -34,21 +28,3 @@ fun EmpoweredRouting() {
     )
     router.renderActivePage { DeferringHost { it() } }
 }
-
-val empoweredMenuItems = listOf(
-    MenuItem.Simple(
-        RoutedMenuItem(
-            path = "/", label = LocalizedText("EmPowerED", "EmPowerED"),
-            pageComponent = { EmpoweredIndex() })
-    ),
-    MenuItem.Simple(
-        RoutedMenuItem(
-            label = LocalizedText("Bronckhorst", "Bronckhorst"),
-            pageComponent = { Bronckhorst() })
-    ),
-    MenuItem.Simple(
-        RoutedMenuItem(
-            label = LocalizedText("Nijmegen Hengstdal", "Nijmegen Hengstdal"),
-            pageComponent = { NijmegenHengstdal() })
-    ),
-)
--- site/src/jsMain/kotlin/domains/lux/subdomains/public_subdomains/empowered/pages/Bronckhorst.kt
+++ site/src/jsMain/kotlin/domains/lux/subdomains/public_subdomains/empowered/pages/Bronckhorst.kt
@@ -1,23 +1,30 @@
 package energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.pages
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.padding
 import energy.lux.frontend.components.widgets.LangText
 import energy.lux.frontend.components.widgets.SectionContainer
+import energy.lux.frontend.core.services.localization.localizedUrl
+import energy.lux.frontend.domains.lux.core.TwinModelCardItem
+import energy.lux.frontend.domains.lux.core.isPrivate
+import energy.lux.frontend.domains.lux.core.model.subdomain.empowered
+import energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.bronckhorstMenuItem
+import energy.lux.frontend.domains.lux.subdomains.public_subdomains.empowered.empoweredDomain
 import energy.lux.frontend.domains.lux.widgets.headings.SubHeaderText
-import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.dom.P
+
+
+val bronckhorstModel = TwinModelCardItem(
+    label = bronckhorstMenuItem.route.label,
+    imageUrl = "/lux/images/bronckhorst.jpg",
+    url = localizedUrl(empoweredDomain, bronckhorstMenuItem.route.path),
+    applicationArea = empowered.applicationArea,
+    isPrivate = empowered.isPrivate
+)
 
 @Composable
 fun Bronckhorst() {
     SectionContainer {
-        SubHeaderText(
-            modifier = Modifier.padding(top = 2.cssRem),
-            enText = "Bronckhorst",
-            nlText = "Bronckhorst",
-        )
-
+        SubHeaderText(nlText = bronckhorstModel.label.nl)
         P {
             LangText(
                 nl = "Kom binnenkort terug om meer te zien.",
