104d6c20aa09980641a6b0efa73f906c623301f2
kofi-k <kofivincent026@gmail.com>
2026-08-12 12:54:00 +0000

    fix reversed ProjectRowCard not applying in test environment

--- site/src/jsMain/kotlin/domains/zenmo/pages/projects/ProjectsStyles.kt
+++ site/src/jsMain/kotlin/domains/zenmo/pages/projects/ProjectsStyles.kt
@@ -1,12 +1,15 @@
 package energy.lux.frontend.domains.zenmo.pages.projects
 
+import com.varabyte.kobweb.compose.css.AlignItems
 import com.varabyte.kobweb.compose.css.ObjectFit
 import com.varabyte.kobweb.compose.css.Overflow
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import energy.lux.frontend.domains.lux.styles.responsiveGap
 import energy.lux.frontend.theme.SitePalette
+import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.FlexDirection
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
@@ -14,7 +17,11 @@ import org.jetbrains.compose.web.css.px
 val ProjectRowReverseStyle = CssStyle {
     base {
         Modifier
+            .fillMaxWidth()
+            .display(DisplayStyle.Flex)
             .flexDirection(FlexDirection.Column)
+            .alignItems(AlignItems.Center)
+            .responsiveGap()
     }
     Breakpoint.LG {
         Modifier
