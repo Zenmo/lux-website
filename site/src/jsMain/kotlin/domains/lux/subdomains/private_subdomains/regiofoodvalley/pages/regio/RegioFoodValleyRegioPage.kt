49b2008ffa2c20b7d74c1e9f7f187905916d092d
kofi-k <kofivincent026@gmail.com>
2026-08-12 18:55:15 +0000

    regiofoodvalley regio model and page

---
+++ site/src/jsMain/kotlin/domains/lux/subdomains/private_subdomains/regiofoodvalley/pages/regio/RegioFoodValleyRegioPage.kt
@@ -0,0 +1,8 @@
+package energy.lux.frontend.domains.lux.subdomains.private_subdomains.regiofoodvalley.pages.regio
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegioFoodValleyRegioPage() {
+    RegioFoodValleyModelPage(twin = regioFoodValleyRegio)
+}
