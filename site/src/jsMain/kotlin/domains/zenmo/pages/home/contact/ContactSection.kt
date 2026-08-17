53f11a911939dad98d2776abf8e5893900012e3d
kofi-k <kofivincent026@gmail.com>
2026-08-17 17:37:36 +0000

    text for ContactSection of homepage

--- site/src/jsMain/kotlin/domains/zenmo/pages/home/contact/ContactSection.kt
+++ site/src/jsMain/kotlin/domains/zenmo/pages/home/contact/ContactSection.kt
@@ -4,20 +4,21 @@ import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.modifiers.alignItems
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.style.toModifier
+import energy.lux.frontend.components.widgets.LangText
 import energy.lux.frontend.components.widgets.SectionContainer
-import energy.lux.frontend.domains.lux.styles.ResponsiveFlexStyle
 import energy.lux.frontend.domains.lux.widgets.headings.HeaderText
-import energy.lux.frontend.domains.zenmo.components.PlaceholderText
 import energy.lux.frontend.domains.zenmo.components.ZenmoContactDetails
 import energy.lux.frontend.domains.zenmo.components.ZenmoLocationMap
 import energy.lux.frontend.domains.zenmo.pages.contact.ContactLocationLayoutStyle
 import energy.lux.frontend.domains.zenmo.pages.models.constrainedWidth
 import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.P
 
 @Composable
 fun ContactSection() {
@@ -42,7 +43,14 @@ private fun ContactInfo() {
     ) {
         HeaderText(enText = "Get In Touch", nlText = "Neem Contact Op")
         P {
-            PlaceholderText()
+            LangText(
+                en = """
+                    Feel free to email us for a talk without any obligations about what Zenmo can do for you. 
+                """.trimIndent(),
+                nl = """
+                    Mail ons gerust voor een vrijblijvend gesprek over de mogelijkheden. 
+                """.trimIndent()
+            )
         }
         ZenmoContactDetails()
     }
